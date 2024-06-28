853c1300964d4189bf3137dca0c5698a41c0bfb9
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-06-28 21:19:57 +0500

    js

--- app.js
+++ app.js
@@ -1,49 +1,62 @@
 
-let num1 = prompt('num1ni kiriting')
-let num2 = prompt('num2ni kiriting')
-let num3 = prompt('num3ni kiriting')
 
-if(num1 > num2 && num1 > num3){
-    console.log(num1)
-}else if(num2 > num1 && num2 > num3){
-    console.log(num2)
-}else{
-    console.log(num3)
-}
+    //  vazifa-1
 
-
-let price = prompt('mablag’ini kiriting')
+// let price = prompt('mablag’ini kiriting')
  
-if (price >= 1000) {
-    console.log('lux')
-} else if (price >= 500 && price <= 1000) {
-    console.log('yarim lux')
-} else if (price >= 500 && price <= 200) {
-    console.log('oddiy')
-}else {
-    console.log('joy yo’q')
-}
+// if (price >= 1000) {
+//      console.log('lux')
+// } else if (price >= 500 && price <= 1000) {
+//     console.log('yarim lux')
+// } else if (price >= 500 && price <= 200) {
+//     console.log('oddiy')
+// }else {
+//     console.log('joy yo’q')
+// }
 
-let num4 = prompt('num4ni kiriting')
 
-if (num4 % 3 == 0 && num4 % 5 == 0) {
-    console.log('fizz-bazz')
-} else if (num4 % 3 == 0) {
-    console.log('fizz')
-} else if (num4 % 5 == 0) {
-    console.log('bazz')
-} else {
-    console.log('joy yo’q')
-}
+    // vazifa-2
 
-let num5 = prompt('num5ni kiriting')
-let num6 = prompt('num6ni kiriting')
-let num7 = prompt('num7ni kiriting')
+// let num4 = prompt('num4ni kiriting')
 
-if(num5 < num6 && num5 < num7){
-    console.log(num5)
-}else if(num6 < num5 && num6 < num7){
-    console.log(num6)
-}else{
-    console.log(num7)
-}
+// if (num4 % 3 == 0 && num4 % 5 == 0) {
+//     console.log('fizz-bazz')
+// } else if (num4 % 3 == 0) {
+//     console.log('fizz')
+// } else if (num4 % 5 == 0) {
+//     console.log('bazz')
+// } else {
+//     console.log('joy yo’q')
+
+// }
+        //   vazifa-4
+
+// let num5 = prompt('num5ni kiriting')
+// let num6 = prompt('num6ni kiriting')
+// let num7 = prompt('num7ni kiriting')
+
+// if(num5 < num6 && num5 < num7){
+//     console.log(num5)
+// }else if(num6 < num5 && num6 < num7){
+//     console.log(num6)
+// }else{
+//     console.log(num7)
+// }
+
+// let son = 5
+
+
+
+        // vazifa-5
+
+// let namber1 = prompt('num1ni kiriting')
+// let namber2 = prompt('num2ni kiriting')
+// let namber3 = prompt('num3ni kiriting')
+
+// if(namber1 > namber2 && namber1 > namber3){
+//     console.log(namber1)
+// }else if(namber2 > namber1 && namber2 > namber3){
+//     console.log(namber2)
+// }else{
+//     console.log(namber3)
+// }
